343810d36b8ed76554386865b150a0ff3b35cd2e
robinson Roy <robinson.roy@me.com>
2016-11-22 10:51:31 +0100

    Exo files

---
+++ js/variables.js
@@ -0,0 +1,68 @@
+var types = [
+{
+    name: 'Margherita',
+    description: {
+        body: "Tomate, mozzarella, basilic, huile d'olive",
+        show: false
+    },
+    price: 8
+},
+{
+    name: 'Reine',
+    description: {
+        body: "Mozzarella, champignons, jambon, olives, huile d'olive",
+        show: false
+    },
+    price: 10
+},
+{
+    name: 'Quattro stagioni',
+    description: {
+        body: "Tomate, mozzarella, champignons, artichaut, poivrons, basilic, huile d'olive",
+        show: false
+    },
+    price: 12
+},
+{
+    name: 'Sicilienne',
+    description: {
+        body: "Tomate, mozzarella, basilic, anchois, câpres, champignon",
+        show: false
+    },
+    price: 11
+}
+];
+
+var pates = [
+{
+    name: 'Classique',
+    price: 0
+},
+{
+    name: 'Extra fine',
+    price: 2
+},
+{
+    name: 'Epaisse',
+    price: 3
+}
+];
+
+var extras = [
+{
+    name: 'Olives',
+    price: 1
+},
+{
+    name: 'Anchois',
+    price: 4
+},
+{
+    name: 'Chèvre',
+    price: 3
+},
+{
+    name: 'Piment',
+    price: 1
+}
+];
