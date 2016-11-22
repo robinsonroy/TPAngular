699aa5914a97dac815ef861a32a5c451f1973061
robinson Roy <robinson.roy@me.com>
2016-11-22 22:17:01 +0100

    two bonus

--- js/app.js
+++ js/app.js
@@ -13,7 +13,7 @@ app.controller('appController', function($scope){
 
   $scope.userChoose =
     {
-      nbParts : '',
+      nbParts : 1,
       type : 'Margherita',
       pate : 'Classique',
       extras : [
@@ -25,6 +25,32 @@ app.controller('appController', function($scope){
 
   $scope.next = false;
 
+  $scope.sliceArray = function(nbParts){
+    var array = [];
+    var j = 0;
 
+    for (var i = 0; i < parseInt(nbParts/6); i++) {
+      array.push(6);
+      j++;
+    }
+    nbParts = nbParts-(j*6);
+    if(nbParts != 0)
+      array.push(nbParts);
+
+    return array;
+  }
+
+  $scope.address = 0;
+  $scope.countAdresse = function(){
+    $scope.address++ ;
+  }
+
+  $scope.getArrayFormNumber = function(num) {
+    var array = [];
+    for (var i = 0; i < num; i++) {
+      array.push(i);
+    }
+    return array;
+  }
 
 });
